060a8f6ff7a5e514a46b078cc92470a16afc2b97
yuheng <15757856573@163.com>
2018-04-23 11:13:41 +0800

    -init project

--- README.md
+++
@@ -1,2 +0,0 @@
-# WXTest
-wxprojectTest
---
+++ pages/buy/buy.js
@@ -0,0 +1,82 @@
+// pages/buy/buy.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    buyList:[{
+      "isChosed":true,
+      "productImgUrl":"http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+      "productName":"商品名称1",
+      "productDesc":"商品描述",
+      "isSpecialOffer":true,
+      "unitPrice":"38",
+      "count":8,
+    }, {
+      "isChosed": true,
+      "productImgUrl": "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+      "productName": "商品名称2",
+      "productDesc": "商品描述",
+      "isSpecialOffer": true,
+      "unitPrice": "58",
+      "count": 9,
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
